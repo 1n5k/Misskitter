329ba2b9aa5eed0f5f2f640a2582536bb604b2d4
Jinsuke <jnsk@jnsk.info>
2018-06-09 23:28:20 +0900

    Learning Android Activity

--- app/src/main/java/info/jnsk/www/misskitter/MainActivity.kt
+++ app/src/main/java/info/jnsk/www/misskitter/MainActivity.kt
@@ -1,10 +1,14 @@
 package info.jnsk.www.misskitter
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.net.*
+import android.widget.Button
+import android.widget.Toast
 
+const val MY_REQUEST_CODE = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +16,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Configuration behavior button
+        val button: Button = findViewById(R.id.button3)
+        button.setOnClickListener {
+
+            // Configuration value to SubActivity
+            val intent: Intent = Intent(this,SubActivity::class.java)
+            intent.putExtra("number",120)
+            intent.putExtra("string","The message from MainActivity")
+
+            // Open SubActivity
+            startActivityForResult(intent, MY_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val received = data!!
+            Toast.makeText(this,"${received.extras.get("number")}, ${received.extras.get("string")}", Toast.LENGTH_LONG).show()
+        }
     }
     val uri: Uri = Uri.parse("https://misskey.xyz/")
-    //val intent  = Intent(Intent.ACTION_VIEW::class.java,uri)
+
 
 }
+
+
--- app/src/main/java/info/jnsk/www/misskitter/SubActivity.kt
+++ app/src/main/java/info/jnsk/www/misskitter/SubActivity.kt
@@ -3,6 +3,31 @@ package info.jnsk.www.misskitter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.net.*
+import android.app.Activity
+import android.content.Intent
+import android.widget.Button
+import android.widget.Toast
 
 class SubActivity:  AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        // Display passed value from MainActivity
+        Toast.makeText(this,"${intent.extras.get("number")},${intent.extras.get("string")}",Toast.LENGTH_LONG).show()
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener() {
+            // Configuration pass value
+            val intent: Intent = Intent()
+            intent.putExtra("number", 300)
+            intent.putExtra("string", "The message from SubActivity")
+
+            // Call onActivityResult of MainActivity with SubActivity information
+            setResult(Activity.RESULT_OK,intent)
+
+            // Close SubActivity
+            finish()
+        }
+    }
 }
